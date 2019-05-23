54a3b22faa9923b185ee44d6106efcdea1c0473e
Eric Lopez <e_lo28@yahoo.com>
2019-05-22 23:30:22 -0700

    setup test database

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-
+  config.active_storage.service = :amazon_dev
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
